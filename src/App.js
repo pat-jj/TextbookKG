eb72f06e41fa0494839a6d80b04d838d77af8727
Patrick Jiang <rexjiang@fuji.waseda.jp>
2023-02-13 16:18:45 -0600

    v0.1 complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
+import { saveAs } from 'file-saver';
 
 // GraphGPT Module
 
@@ -46,7 +47,8 @@ function App() {
     })
   };
 
-  const updateGraph = (updates) => {
+
+  const updateGraph = async (updates) => {
     // updates will be provided as a list of lists
     // each list will be of the form [ENTITY1, RELATION, ENTITY2] or [ENTITY1, COLOR]
 
@@ -126,7 +128,7 @@ function App() {
     setGraphState(current_graph);
   };
 
-  const queryStatelessPrompt = (prompt, apiKey) => {
+  const queryStatelessPrompt = async (prompt, apiKey) => {
     fetch('prompts/stateless.prompt')
       .then(response => response.text())
       .then(text => text.replace("$prompt", prompt))
@@ -167,17 +169,16 @@ function App() {
 
             updateGraph(updates);
 
-            document.getElementsByClassName("searchBar")[0].value = "";
-            document.body.style.cursor = 'default';
-            document.getElementsByClassName("generateButton")[0].disabled = false;
+            // document.getElementsByClassName("searchBar")[0].value = "";
+            // document.getElementsByClassName("generateButton")[0].disabled = false;
           }).catch((error) => {
             console.log(error);
-            alert(error);
+            // alert(error);
           });
       })
   };
 
-  const queryStatefulPrompt = (prompt, apiKey) => {
+  const queryStatefulPrompt = async (prompt, apiKey) => {
     fetch('prompts/stateful.prompt')
       .then(response => response.text())
       .then(text => text.replace("$prompt", prompt))
@@ -218,21 +219,21 @@ function App() {
 
             setGraphState(new_graph);
 
-            document.getElementsByClassName("searchBar")[0].value = "";
+            // document.getElementsByClassName("searchBar")[0].value = "";
             document.body.style.cursor = 'default';
             document.getElementsByClassName("generateButton")[0].disabled = false;
           }).catch((error) => {
             console.log(error);
-            alert(error);
+            // alert(error);
           });
       })
   };
 
-  const queryPrompt = (prompt, apiKey) => {
+  const queryPrompt = async (prompt, apiKey) => {
     if (SELECTED_PROMPT === "STATELESS") {
-      queryStatelessPrompt(prompt, apiKey);
+      await queryStatelessPrompt(prompt, apiKey);
     } else if (SELECTED_PROMPT === "STATEFUL") {
-      queryStatefulPrompt(prompt, apiKey);
+      await queryStatefulPrompt(prompt, apiKey);
     } else {
       alert("Please select a prompt");
       document.body.style.cursor = 'default';
@@ -251,11 +252,77 @@ function App() {
     queryPrompt(prompt, apiKey);
   }
 
+  const handleSave = (save_path) => {
+    const blob = new Blob([JSON.stringify(graphState, null, 2)], { type: 'application/json;charset=utf-8' });
+    saveAs(blob, save_path);
+  };
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  const resumeGraph = () => {
+    let file_path = `./knowledge_graphs/${selectedSection.replaceAll(" ", "_")}.json`;
+      
+    // TODO: connect Google Drive
+    fetch(file_path, { method: 'HEAD' })
+      .then(response => {
+        if (response.ok) {
+          console.log(`The file "${file_path}" exists.`);
+        } else {
+          console.log(`The file "${file_path}" does not exist.`);
+        }
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+
+      });
+
+    fetch(file_path)
+      .then(response => response.json())
+      .then(graph => setGraphState(graph));
+  
+  }
+
+  const outputGraph = () => {
+    handleSave(`${selectedSection}.json`);
+  }
+
+  const regenerateGraph = async () => {
+    document.body.style.cursor = 'wait';
+    document.getElementsByClassName("generateButton")[0].disabled = true;
+    const apiKey = document.getElementsByClassName("apiKeyTextField")[0].value;
+  
+    if (selectedText.length === 0) {
+      document.body.style.cursor = 'default';
+      console.log("No selected quality text to prompt the graph ...");
+    }
+    
+    let i = 0;
+    
+    const callQueryPrompt = async () => {
+      if (i < selectedText.length) {
+        console.log(selectedText[i]);
+        await queryPrompt(selectedText[i], apiKey);
+        i++;
+        await callQueryPrompt();
+      
+      } else {
+        console.log("Wait graph to be updated ... (10s)")
+        await delay(10000);
+        handleSave(`${selectedSection}.json`);
+        document.body.style.cursor = 'default'; 
+        document.getElementsByClassName("generateButton")[0].disabled = false;
+      }
+    };
+    await callQueryPrompt(); 
+    
+  }
+
   //
   // Start Textbook PDF Viewer module
   //
 
   const [selectedChapter, setSelectedChapter] = useState(null);
+  const [selectedSection, setSelectedSection] = useState(null);
+  const [selectedText, setSelectedText] = useState([null]);
   const [inSections, setInSections] = useState([]);
 	const [numPages, setNumPages] = useState(null);
 	const [pageNumber, setPageNumber] = useState(1);
@@ -276,6 +343,55 @@ function App() {
       setInSections(Object.entries(selectedChapter["in_sections"]));
     }
   }, [selectedChapter]);
+
+  useEffect(() => {
+    if (selectedChapter && pageNumber) {
+      let selectedSection;
+      let inSections = Object.entries(selectedChapter["in_sections"]);
+      for (let i = 0; i < inSections.length; i++) {
+        let currentSection = inSections[i];
+        let nextSection = inSections[i + 1];
+        if (nextSection && pageNumber >= currentSection[1] && pageNumber < nextSection[1]) {
+          selectedSection = currentSection[0];
+          break;
+        } else if (!nextSection && pageNumber >= currentSection[1]) {
+          selectedSection = currentSection[0];
+          break;
+        }
+      }
+      setSelectedSection(selectedSection);
+    }
+  }, [pageNumber, selectedChapter]);
+
+  // Use "===" to label the text we want to convert
+  function extractTextBetweenDelimiters(text) {
+    let regex = /===([\s\S]*?)===/g;
+    let matches = [];
+    let match;
+  
+    while ((match = regex.exec(text)) !== null) {
+      matches.push(match[1].trim().replaceAll('\n', ' '));
+    }
+  
+    return matches;
+  }
+
+  useEffect(() => {
+    if (selectedSection) {
+      let chapter_num = Array.from(Array.from(Array.from(Object.entries(selectedChapter["in_sections"]))[0])[0])[0];
+      let chapter_name = `CHAPTER_${chapter_num}_${selectedChapter.name}`;
+      let file_path = `./textbooks/${chapter_name}/${selectedSection.replaceAll(" ", "_")}.txt`;
+      
+      console.log(chapter_name);
+      console.log(selectedSection);
+      console.log(file_path)
+      fetch(file_path)
+      .then(response => response.text())
+      .then(TEXT => setSelectedText(extractTextBetweenDelimiters(TEXT)));
+
+      console.log(selectedText);
+    }
+  }, [selectedSection]);
 
 	const onDocumentLoadSuccess = ({ numPages }) => {
 		setNumPages(numPages);
@@ -311,9 +427,19 @@ function App() {
       });
   };
 
-  const handleSectionSelect = (eventKey) => {
+  const handleSectionSelectPage = (eventKey) => {
     let page_num = parseInt(eventKey)
     setPageNumber(page_num);
+  };
+
+  const handleSectionSelectKey = (eventKey) => {
+    setSelectedSection(eventKey.replaceAll(' ', '_'));
+    console.log(selectedSection); 
+  }
+
+  const handleSectionSelect = (eventKey) => {
+    handleSectionSelectPage(eventKey.split('_')[1]);
+    handleSectionSelectKey(eventKey.split('_')[0]);
   };
 
 	return (
@@ -329,7 +455,7 @@ function App() {
               </DropdownButton>{" "}
               <DropdownButton onSelect={handleSectionSelect} id="dropdown-basic-button" title="Select Section">
               {inSections.map((section) => (
-                <Dropdown.Item key={section[0]} eventKey={section[1]}>
+                <Dropdown.Item key={section[0]} eventKey={section[0] + '_' + section[1]}>
                   {section[0]}
                 </Dropdown.Item>
               ))}
@@ -347,30 +473,34 @@ function App() {
           </nav>
 
             <p>
-              Page {pageNumber} of {numPages}
+            ✨ Page {pageNumber} of {numPages} ✨ <span style={{ fontWeight: 'bold' }}>{selectedSection}</span>
             </p>
             
           <Document
             file={pdfFile}
             onLoadSuccess={onDocumentLoadSuccess}
           >
-            <Page scale={1.5} pageNumber={pageNumber} />
+            <Page scale={1.3} pageNumber={pageNumber} />
           </Document>
       </div>
         <div className='knowledge_graph' style={{ flex: 1.5 }}>
-          <h1 className="headerText">TextbookKG 📖</h1>
+        <nav>
+          <h1 className="headerText"><img src={require('./logo.png')} width="200" height="180" /> TextbookKG </h1>
+        </nav>
           {/* <p className='subheaderText'>Build complex, directed graphs to add structure to your ideas using natural language. Understand the relationships between people, systems, and maybe solve a mystery.</p> */}
-          {/* <center>
-            <div className='inputContainer'>
-              <input className="searchBar" placeholder="Describe your graph..."></input>
-              <input className="apiKeyTextField" type="password" placeholder="Enter your OpenAI API key..."></input>
-              <button className="generateButton" onClick={createGraph}>Generate</button>
-              <button className="clearButton" onClick={clearState}>Clear</button>
-            </div>
-          </center> */}
           <div className='graphContainer'>
             <Graph graph={graphState} options={options} style={{ height: "640px" }} />
           </div>
+          <center>
+            <div className='inputContainer'>
+              {/* <input className="searchBar" placeholder="Describe your graph..."></input> */}
+              <button className="resumeButton" onClick={resumeGraph}>Resume</button>
+              <button className="generateButton" onClick={regenerateGraph}>Re-generate (Update)</button>
+              <button className="outButton" onClick={outputGraph}>Output</button>
+              <button className="clearButton" onClick={clearState}>Clear</button>
+              <input className="apiKeyTextField" type="password" placeholder="Enter your OpenAI API key..."></input>
+            </div>
+          </center>
           <p className='footer'>Pro tip: don't take a screenshot! You can right-click and save the graph as a .png  📸</p>
         </div>
 		</div>
