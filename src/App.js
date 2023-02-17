118b5d418b8a443efb52d33bce2dc8214a63830c
Patrick Jiang <rexjiang@fuji.waseda.jp>
2023-02-16 20:50:37 -0600

    minor

--- src/App.js
+++ src/App.js
@@ -512,8 +512,8 @@ function App() {
 
   const responseGoogle = (response) => {
     console.log(response);
-    if (response.tokenObj && response.tokenObj.access_token) {
-      const token = response.tokenObj.access_token;
+    if (response.tokenObj && response.tokenObj.credential) {
+      const token = response.tokenObj.credential;
       setCredentialResponse(response);
       setAccessToekn(token);
     } else {
